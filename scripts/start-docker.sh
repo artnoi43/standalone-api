b0f59e3fd31b616f7631e5031b443067bca352a6
PP <artdrawin@gmail.com>
2021-11-07 02:21:37 +0700

    new posrgres docker name

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 5432:5432 --name postgres13 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d postgres:13-alpine
+docker run -p 5432:5432 --name standalone-api-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d postgres:13-alpine
